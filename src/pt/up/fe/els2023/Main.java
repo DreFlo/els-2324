961b30e3682904dedb472bc035cd3f47339771dd
Tiago Rodrigues <tiaguito.andre21764@gmail.com>
2023-09-19 19:44:30 +0100

    Yaml Parser imported

--- src/pt/up/fe/els2023/Main.java
+++ src/pt/up/fe/els2023/Main.java
@@ -1,8 +1,24 @@
 package pt.up.fe.els2023;
 
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Map;
+
+
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		Yaml yaml = new Yaml();
+		InputStream inputStream = null;
+		try {
+			inputStream = new FileInputStream(args[0]);
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+		Map<String, Object> obj = yaml.load(inputStream);
+		System.out.println(obj);
 	}
 }
