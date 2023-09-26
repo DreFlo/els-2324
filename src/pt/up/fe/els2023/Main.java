7aca7d9bb780d32ac0a8531f70f80adfa404e9fb
André Flores <andrejesusferflores@gmail.com>
2023-09-26 19:46:04 +0100

    Remove unused imports

--- src/pt/up/fe/els2023/Main.java
+++ src/pt/up/fe/els2023/Main.java
@@ -1,12 +1,8 @@
 package pt.up.fe.els2023;
 
-
-import pt.up.fe.els2023.Config.TableConfig;
 import pt.up.fe.els2023.Controller.Controller;
-import pt.up.fe.els2023.FileParser.ConfigFileParser.JSONConfigFileParser;
 
 import java.io.File;
-import java.util.List;
 
 public class Main {
 	
