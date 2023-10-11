75763a735e99517bee27c97ac949eb94fd8ecf64
André Flores <andrejesusferflores@gmail.com>
2023-10-11 17:11:10 +0100

    Bug fix: wrong key for path

--- src/pt/up/fe/els2023/Config/Source/SourceBuilder.java
+++ src/pt/up/fe/els2023/Config/Source/SourceBuilder.java
@@ -5,8 +5,8 @@ import java.util.Map;
 public class SourceBuilder {
     public static Source getSource(String type, Map<?, ?> params) throws IllegalArgumentException{
         return switch (type) {
-            case "file" -> new FileSource((String) params.get("pathPattern"));
-            case "folder" -> new FolderSource((String) params.get("pathPattern"));
+            case "file" -> new FileSource((String) params.get("path"));
+            case "folder" -> new FolderSource((String) params.get("path"));
             default -> throw new IllegalArgumentException("Invalid source type");
         };
     }
